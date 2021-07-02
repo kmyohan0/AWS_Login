21b1a93ea97171586ff7a0f68d197a35de78de6b
FranciscoHu17 <francisco.hu17@gmail.com>
2021-07-02 09:54:02 -0400

    created intial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Home from './components/home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Login from './components/login';
+import Employee from './components/employee';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component{
+
+    render(){
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/'> <Home /> </Route>
+                    <Route path='/login'> <Login /> </Route>
+                    <Route path='/employee'> <Employee /> </Route>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/login.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+                        username: '',
+                        password: '',
+                    }
+    }
+
+    changeHandler = (event) => {
+        switch(event.target.name){
+            case 'username':
+                this.setState({username: event.target.value});
+                break;
+            case 'password':
+                this.setState({password: event.target.value});
+                break;
+            default:
+                this.setState({username: '', password: ''});
+        }
+    }
+
+    submitHandler = (event) =>{
+        alert('username: ' + this.state.username + '\npassword: ' + this.state.password);
+        event.preventDefault();
+    }
+
+    render(){
+        return (
+            <div className = 'login'>
+                <h1>Login Page</h1>
+                <form onSubmit={this.submitHandler}>
+                    <label>Username:</label><br/>
+                    <input type='text' name='username' onChange={this.changeHandler}></input><br/><br/>
+
+                    <label>Password:</label><br/>
+                    <input type='password' name='password' onChange={this.changeHandler}></input><br/><br/>
+
+                    <input type='submit'></input>
+                </form>
+            </div>     
+        );
+    }
+}
+
+export default Login;
